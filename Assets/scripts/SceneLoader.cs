8ba3077a699037fadf158198f75ebf924b1bf265
Anubis Lockward <anubis.lockward@gmail.com>
2020-11-19 22:01:14 -0400

    Added SceneLoader to all Scenes

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = GetCurrentSceneIndex();
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private static int GetCurrentSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+}
