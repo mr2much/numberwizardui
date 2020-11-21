b96c59a7028f3bac9b86966c55b4dae7bc4f7ee2
Anubis Lockward <anubis.lockward@gmail.com>
2020-11-20 20:16:36 -0400

    Refactored GamePlay script to work with UI

--- Assets/scripts/NumberWizard.cs
+++ Assets/scripts/NumberWizard.cs
@@ -4,91 +4,43 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    private static string name = "Anubis";
+    private string name = "Anubis";
     [SerializeField] public int highestNumber;
     [SerializeField] public int lowestNumber;
-    private static int guess;
-    private static int previousGuess;
+    private int guess;
+    private int previousGuess;
 
     // Start is called before the first frame update
     void Start()
-    {        
-        GreetPlayer();
-        ResetGame();
-    }
-
-    private void GreetPlayer()
     {
-        Debug.Log("Halt mortal! Who goes there?");
-        Debug.Log("So " + name + ", get ready to be amazed by tne Number Wizard!");
-        Debug.Log("Pick a number and prepare to have your brain blown!");
+        ResetGame();
     }
 
     private void ResetGame()
     {
-        bool keepGoing = true;
-        highestNumber = 1000;
-        lowestNumber = 1;
-        previousGuess = 0;
-        Debug.Log("The highest number you can pick is: " + highestNumber);
-        Debug.Log("The lowest number you can pick is: " + lowestNumber);
-        CalculateNextGuess();        
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push ENTER = Correct!");
-        highestNumber += 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Detect which key was pressed
-        foreach (KeyCode keyPressed in System.Enum.GetValues(typeof(KeyCode)))
-        {
-            if (Input.GetKeyDown(keyPressed))
-            {
-                switch (keyPressed)
-                {
-                    case KeyCode.Return:
-                        Debug.Log("I knew it from the very beginning!");
-                        ResetGame();
-                        break;
-                    case KeyCode.UpArrow:
-                        lowestNumber = guess;
-                        CalculateNextGuess();
-
-                        break;
-                    case KeyCode.DownArrow:
-                        highestNumber = guess;
-                        CalculateNextGuess();
-                        break;
-                    case KeyCode.Escape:
-                        Debug.Log("Later loser!");
-                        break;
-                    default:
-                        Debug.Log("I haven't got all day for this");
-                        break;
-                }
-            }
-        }
-    }
+        highestNumber += 1;        
+        CalculateNextGuess();
+    }    
 
     public void OnPressHigher()
     {
-
+        lowestNumber = guess;
+        CalculateNextGuess();
     }
 
     public void OnPressLower()
     {
-
+        highestNumber = guess;
+        CalculateNextGuess();
     }
 
-    private static void CalculateNextGuess()
+    private void CalculateNextGuess()
     {
         guess = (highestNumber + lowestNumber) / 2;
-        Debug.Log("Now, is your number is higher or lower than: " + guess);
 
         if (guess == previousGuess)
         {
-            Debug.Log("Okay, now you are just wasting my time!");
+            
         }
         else
         {
